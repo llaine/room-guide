f1b268c1d949f8c8b20e10b763c8636496dca25f
Jérémy Morin <hi@jermor.in>
2016-03-09 19:39:12 +0100

    feat(init) clear bullshit and transform to use case

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function () {
+    var self = this;
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    self.rooms = [
+      {title: 'Room1', id: 1},
+      {title: 'Room2', id: 2},
+      {title: 'Room3', id: 3},
+      {title: 'Room4', id: 4},
+      {title: 'Room5', id: 5},
+      {title: 'Room6', id: 6}
+    ];
+  })
 
-  // Form data for the login modal
-  $scope.loginData = {};
+  .controller('RoomCtrl', function ($scope, $stateParams) {
+    var self = this;
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    self.roomId = $stateParams.roomId;
+
+    self.records = [
+      {title: 'Record1', id: 1},
+      {title: 'Record2', id: 2},
+      {title: 'Record3', id: 3},
+      {title: 'Record4', id: 4},
+      {title: 'Record5', id: 5},
+      {title: 'Record6', id: 6}
+    ];
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
