cdf5880237c85f6b2edab20d8b1213073628e21c
llaine <darksioul6@gmail.com>
2016-03-10 13:41:34 +0100

    feat(factory): Moving audio songs fetching in specific factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,23 +13,9 @@ angular.module('starter.controllers', [])
     ];
   })
 
-  .controller('RoomCtrl', function ($scope, $stateParams) {
+  .controller('RoomCtrl', function ($scope, $stateParams, AudioFactory) {
     var self = this;
 
     self.roomId = $stateParams.roomId;
-
-    self.tracks = [
-      {
-        url: 'https://ionic-audio.s3.amazonaws.com/Message%20in%20a%20bottle.mp3',
-        artist: 'The Police',
-        title: 'Message in a bottle',
-        art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-      },
-      {
-        url: 'https://ionic-audio.s3.amazonaws.com/Roxane.mp3',
-        artist: 'The Police',
-        title: 'Roxane',
-        art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-      }
-    ];
+    self.tracks = AudioFactory.getSongsForRoom($stateParams.roomId)
   });
